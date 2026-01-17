62bc877c71d46f930376373d6221ca1fa1d58bb7
雷宇 <1299892569@qq.com>
2026-01-18 00:11:23 +0800

    Site updated: 2026-01-18 00:11:22

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/17/csgo-gaming-tips/","2025/01/17/frontend-learning-notes/","2025/12/23/hello-world/","2025/01/17/my-first-blog/"];function toRandomPost(){
+var posts=["2025/01/17/csgo-gaming-tips/","2025/01/17/frontend-learning-notes/","2025/12/23/hello-world/","2025/01/17/how-to-write-blog/","2025/01/17/my-first-blog/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
